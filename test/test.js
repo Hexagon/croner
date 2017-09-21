13bb16144e1a28112e0c11a19509aefa558761da
Hexagon <robinnilsson@gmail.com>
2017-09-21 20:38:07 +0200

    Release 1.1.21, further improvements

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2015 Hexagon <robinnilsson@gmail.com>
+Copyright (c) 2015-2017 Hexagon <robinnilsson@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
