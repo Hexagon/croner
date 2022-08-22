3eb0c82ac15981946b9d736cc039b33fa2b2ea7e
Hexagon <hexagon@56k.guru>
2022-08-22 21:18:37 +0200

    Increase to 365 days in day iteration test, test for #132

--- test/node/js/src/suites/basics.cjs
+++ test/node/js/src/suites/basics.cjs
@@ -505,11 +505,11 @@ module.exports = function (Cron, test) {
 		});
 	}));
 
-	test("0 0 0 * * * with 40 iterations should return 40 days from now", function () {
+	test("0 0 0 * * * with 365 iterations should return 365 days from now", function () {
 		let scheduler = new Cron("0 0 0 * * *"),
 			prevRun = new Date(),
 			nextRun,
-			iterations = 40,
+			iterations = 365,
 			compareDay = new Date();
 			
 		compareDay.setDate(compareDay.getDate() + iterations);
@@ -517,6 +517,7 @@ module.exports = function (Cron, test) {
 		while(iterations-->0) {
 			nextRun = scheduler.next(prevRun),
 			prevRun = nextRun;
+			console.log(nextRun);
 		}
 
 		// Set seconds, minutes and hours to 00:00:00
