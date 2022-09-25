920aefe93b2eac0f448ef41a35a5cded6355cdbc
Hexagon <hexagon@56k.guru>
2022-09-25 21:16:20 +0200

    Deno format fix.

--- test/deno/bench/perf.bench.ts
+++ test/deno/bench/perf.bench.ts
@@ -1,17 +1,17 @@
 import { Cron } from "../../../src/croner.js";
 
 Deno.bench("Enumerate 100 seconds", { group: "url" }, () => {
-    new Cron("* * * * * *").enumerate(100);
+  new Cron("* * * * * *").enumerate(100);
 });
 
 Deno.bench("Enumerate 100 sundays (legacy mode)", { group: "url" }, () => {
-    new Cron("0 0 0 * * SUN", { legacyMode: true }).enumerate(100);
+  new Cron("0 0 0 * * SUN", { legacyMode: true }).enumerate(100);
 });
 
 Deno.bench("Enumerate 100 sundays (croner mode)", { group: "url" }, () => {
-    new Cron("0 0 0 * * SUN", { legacyMode: true }).enumerate(100);
+  new Cron("0 0 0 * * SUN", { legacyMode: true }).enumerate(100);
 });
 
 Deno.bench("Enumerate 100 29th of february (croner mode)", { group: "url" }, () => {
-    new Cron("0 0 0 29 2 *", { legacyMode: true }).enumerate(100);
+  new Cron("0 0 0 29 2 *", { legacyMode: true }).enumerate(100);
 });
