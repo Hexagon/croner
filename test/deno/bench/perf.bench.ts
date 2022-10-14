0d2b7d275c6cb04f061debd2f57086504a9dc71a
Hexagon <hexagon@56k.guru>
2022-10-15 01:47:20 +0200

    Optimize pattern parsing. Update benchmark.

--- test/deno/bench/perf.bench.ts
+++ test/deno/bench/perf.bench.ts
@@ -1,17 +1,31 @@
+import { Cron as CronDist } from "../../../dist/croner.min.mjs";
 import { Cron } from "../../../src/croner.js";
 
-Deno.bench("Enumerate 100 seconds", { group: "url" }, () => {
-  new Cron("* * * * * *").enumerate(100);
+Deno.bench("Enumerate 100 seconds, Dev", { group: "first" }, () => {
+  new Cron("* * * * * *").next();
 });
 
-Deno.bench("Enumerate 100 sundays (legacy mode)", { group: "url" }, () => {
-  new Cron("0 0 0 * * SUN", { legacyMode: true }).enumerate(100);
+Deno.bench("Enumerate 100 seconds, Dist", { group: "first" }, () => {
+  new CronDist("* * * * * *").next();
 });
 
-Deno.bench("Enumerate 100 sundays (croner mode)", { group: "url" }, () => {
-  new Cron("0 0 0 * * SUN", { legacyMode: true }).enumerate(100);
+Deno.bench("Enumerate 100 sundays (legacy mode), Dev", { group: "second" }, () => {
+  new Cron("0 0 0 * * SUN", { legacyMode: true }).next();
+});
+Deno.bench("Enumerate 100 sundays (legacy mode), Dist", { group: "second" }, () => {
+  new CronDist("0 0 0 * * SUN", { legacyMode: true }).next();
 });
 
-Deno.bench("Enumerate 100 29th of february (croner mode)", { group: "url" }, () => {
-  new Cron("0 0 0 29 2 *", { legacyMode: true }).enumerate(100);
+Deno.bench("Enumerate 100 sundays (croner mode), Dev", { group: "third" }, () => {
+  new Cron("0 0 0 * * SUN", { legacyMode: true }).next();
+});
+Deno.bench("Enumerate 100 sundays (croner mode), Dist", { group: "third" }, () => {
+  new CronDist("0 0 0 * * SUN", { legacyMode: true }).next();
+});
+
+Deno.bench("Enumerate 100 29th of february (croner mode), Dev", { group: "fourth" }, () => {
+  new Cron("0 0 0 29 2 *", { legacyMode: true }).next();
+});
+Deno.bench("Enumerate 100 29th of february (croner mode), Dist", { group: "fourth" }, () => {
+  new CronDist("0 0 0 29 2 *", { legacyMode: true }).next();
 });
