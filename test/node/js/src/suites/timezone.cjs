c78a9e27ad03a5ceab009be71c4ef5fed31ad2c2
Hexagon <hexagon@56k.guru>
2022-10-26 21:17:16 +0200

    Fix DST double occurrence. Add test.

--- test/node/js/src/suites/timezone.cjs
+++ test/node/js/src/suites/timezone.cjs
@@ -119,4 +119,29 @@ module.exports = function (Cron, test) {
 		assert.equal(Math.abs(nextRun.getTime()-compareDay.getTime())<13*60*60*1000, true);
 
 	});
+
+	test("0 30 2 * * * with 365 iterations should return 365 days from now in Europe/Berlin", function () {
+		let scheduler = new Cron("0 30 2 * * *", { timezone: "Europe/Berlin" }),
+			prevRun = new Date(),
+			nextRun,
+			iterations = 365,
+			compareDay = new Date();
+			
+		compareDay.setDate(compareDay.getDate() + iterations );
+		
+		while(iterations-->0) {
+			nextRun = scheduler.next(prevRun);
+			prevRun = nextRun;
+		}
+
+		// Set seconds, minutes and hours to 00:00:00
+		compareDay.setMilliseconds(0);
+		compareDay.setSeconds(0);
+		compareDay.setMinutes(0);
+		compareDay.setHours(0);
+
+		// Do comparison
+		assert.equal(Math.abs(nextRun.getTime()-compareDay.getTime())<13*60*60*1000, true);
+
+	});
 };
