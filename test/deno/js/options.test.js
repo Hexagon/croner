c8ba933a158fddb87ce0dbe960eb019aa53ee506
Hexagon <hexagon@56k.guru>
2023-02-13 21:17:42 +0100

    Further improve Deno tests. Ready for 5.6.3.

--- test/deno/js/options.test.js
+++ test/deno/js/options.test.js
@@ -2,19 +2,31 @@ import { assertThrows } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { Cron } from "../../../src/croner.js";
 
 Deno.test("Cannot combine utcOffset with timezone", function () {
-  assertThrows(() => {
-    Cron("* * * 31 2 *", { utcOffset: 0, timezone: "Europe/Stockholm" });
-  }, "cannot combine");
+  assertThrows(
+    () => {
+      Cron("* * * 31 2 *", { utcOffset: 0, timezone: "Europe/Stockholm" });
+    },
+    undefined,
+    "Combining",
+  );
 });
 
 Deno.test("utcOffset cannot be out of bounds (upper)", function () {
-  assertThrows(() => {
-    Cron("* * * 31 2 *", { utcOffset: 1800 });
-  }, "cannot combine");
+  assertThrows(
+    () => {
+      Cron("* * * 31 2 *", { utcOffset: 1800 });
+    },
+    undefined,
+    "bounds",
+  );
 });
 
 Deno.test("utcOffset cannot be out of bounds (lower)", function () {
-  assertThrows(() => {
-    Cron("* * * 31 2 *", { utcOffset: -1800 });
-  }, "cannot combine");
+  assertThrows(
+    () => {
+      Cron("* * * 31 2 *", { utcOffset: -1800 });
+    },
+    undefined,
+    "bounds",
+  );
 });
