392398c9dafa619dfee9714a52b75f0bb6d5a460
Hexagon <hexagon@56k.guru>
2024-08-03 21:42:36 +0200

    Fix issue #254 in legacy-node-support branch. Release 7.0.8.

--- types/croner.d.ts
+++ types/croner.d.ts
@@ -255,41 +255,6 @@ export namespace Cron {
  */
 export const scheduledJobs: Cron[];
 /**
- * @callback CatchCallbackFn
- * @param {unknown} e
- * @param {Cron} job
- */
-/**
- * @callback ProtectCallbackFn
- * @param {Cron} job
- */
-/**
- * @typedef {Object} CronOptions - Cron scheduler options
- * @property {string} [name] - Name of a job
- * @property {boolean} [paused] - Job is paused
- * @property {boolean} [kill] - Job is about to be killed or killed
- * @property {boolean | CatchCallbackFn} [catch] - Continue exection even if a unhandled error is thrown by triggered function
- * 										  - If set to a function, execute function on catching the error.
- * @property {boolean} [unref] - Abort job instantly if nothing else keeps the event loop running.
- * @property {number} [maxRuns] - Maximum nuber of executions
- * @property {number} [interval] - Minimum interval between executions, in seconds
- * @property {boolean | ProtectCallbackFn} [protect] - Skip current run if job is already running
- * @property {string | Date} [startAt] - When to start running
- * @property {string | Date} [stopAt] - When to stop running
- * @property {string} [timezone] - Time zone in Europe/Stockholm format
- * @property {number} [utcOffset] - Offset from UTC in minutes
- * @property {boolean} [legacyMode] - Combine day-of-month and day-of-week using true = OR, false = AND. Default is true = OR.
- * @property {?} [context] - Used to pass any object to scheduled function
- */
-/**
- * Internal function that validates options, and sets defaults
- * @private
- *
- * @param {CronOptions} options
- * @returns {CronOptions}
- */
-declare function CronOptions(options: CronOptions): CronOptions;
-/**
  * Converts date to CronDate
  * @constructor
  *
